e96868891271f27ed8dbb6afe9f0d4516bf5c24f
Sean Porter <portertech@gmail.com>
2012-01-10 22:26:39 -0800

    must require float patch for .pretty

--- lib/recognizer/librato.rb
+++ lib/recognizer/librato.rb
@@ -2,6 +2,8 @@ require "rubygems"
 require "thread"
 require "librato/metrics"
 
+require File.join(File.dirname(__FILE__), 'patches', 'float')
+
 module Recognizer
   class Librato
     def initialize(thread_queue, options)
