a98d9364aa6916126b50b47d3062e9cbde8ed980
Sean Porter <portertech@gmail.com>
2012-08-03 11:46:44 -0700

    [jruby] require openssl patch for jruby only

--- lib/recognizer/patches/openssl.rb
+++ lib/recognizer/patches/openssl.rb
@@ -1,8 +1,10 @@
-require "openssl"
+if RUBY_PLATFORM == "java"
+  require "openssl"
 
-module OpenSSL
-  module SSL
-    remove_const(:VERIFY_PEER)
-    const_set(:VERIFY_PEER, VERIFY_NONE)
+  module OpenSSL
+    module SSL
+      remove_const(:VERIFY_PEER)
+      const_set(:VERIFY_PEER, VERIFY_NONE)
+    end
   end
 end
