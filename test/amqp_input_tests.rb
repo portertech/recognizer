1c00401da0664e0d6098a48aa548972f1ae8411d
Sean Porter <portertech@gmail.com>
2012-12-18 20:02:34 -0800

    cleaned up input tests

--- test/amqp_input_tests.rb
+++ test/amqp_input_tests.rb
@@ -5,24 +5,14 @@ class TestAMQPInput < MiniTest::Unit::TestCase
 
   def test_amqp_consumer
     setup_amqp_input
-    sleep 2
-    metrics = [
-      "foo 42 #{Time.now.to_i}",
-      "malformed",
-      "bar 73 #{Time.now.to_i}"
-    ]
     rabbitmq = HotBunnies.connect
     amq = rabbitmq.create_channel
     exchange = amq.exchange("graphite", :type => "topic", :durable => false)
-    metrics.each do |metric|
+    sample_metrics.each do |metric|
       exchange.publish(metric, :routing_key => "recognizer")
     end
-    results = Array.new
-    2.times do
-      results << @input_queue.shift
-    end
-    metrics.delete("malformed")
-    assert_equal(metrics, results)
+    assert_equal(sample_metrics.first, @input_queue.shift)
+    assert_equal(sample_metrics.last, @input_queue.shift)
     exchange.publish("42 #{Time.now.to_i}", :routing_key => "foo")
     assert_equal("foo 42 #{Time.now.to_i}", @input_queue.shift)
     amq.close
